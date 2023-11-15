97fe06143fd304ae938c074aafde1f74a99d35ba
SAM ADKINS <sjamesadkins@gmail.com>
2023-11-14 21:07:15 -0600

    Component practice, props

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {reactDescriptions[genRandomInt(2)]} React concepts you will need for
-        almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
+import CoreConcept from "./components/CoreConcept.jsx";
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+             {/* object destructuring with ...*/}
+          <ul> 
+            <CoreConcept {...CORE_CONCEPTS[0]} /> 
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,11 @@
+function CoreConcept({image, title, description}) {//object destructuring with extra {}
+    return (
+      <li>
+        <img src={image} alt={title} />
+        <h3>{title} </h3>
+        <p>{description}</p>
+      </li>
+    );
+  }
+
+  export default CoreConcept;

5dd5aa7fce43fb087745d50576316d3f4e6ecf51
SAM ADKINS <sjamesadkins@gmail.com>
2024-01-04 19:29:29 -0600

    updated app to add jsx content conditionally with change in State

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+
 import CoreConcept from "./components/CoreConcept.jsx";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data.js"
 
 function App() {
+
+  const [selectedTopic, setSelectedTopic] = useState("");
+
   function handleSelect(selectedButton) {
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
   }
 
   return (
@@ -34,7 +41,14 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          Dynamic Content
+            {/* content rendered conditionally with ternary expression */}
+            {!selectedTopic ? <p>Please select a topic.</p> : <div id = "tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+            </div>}
         </section>
       </main>
     </div>

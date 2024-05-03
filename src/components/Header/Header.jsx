e160ea40125b72d90c51c93f73dd40a81c698a64
SAM ADKINS <sjamesadkins@gmail.com>
2024-05-03 11:20:10 -0500

    finished project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,6 +16,7 @@ function Header() {
           {reactDescriptions[genRandomInt(2)]} React concepts you will need for
           almost any app you are going to build!
         </p>
+
       </header>
     );
   }
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -7,11 +7,11 @@
 //   );
 // }
 
-export default function TabButton({ children, onSelect, isSelected }) {
+export default function TabButton({ children, isSelected, ...props }) {
 
     return (
       <li>
-        <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+        <button className={isSelected ? 'active' : undefined} {...props}>{children}</button>
       </li>
     );
   }
